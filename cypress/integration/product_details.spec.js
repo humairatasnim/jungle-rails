962144decedc23c0ed708d1827fec1617b379637
Humaira Tasnim <hxtasnim@gmail.com>
2023-05-30 09:34:44 -0400

    Add cypress tests for product details

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -5,7 +5,10 @@ describe('jungle rails app', () => {
   it("There are products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
-  it("There is 2 products on the page", () => {
-    cy.get(".products article").should("have.length", 2);
+  it("Navigates to product detail page", () => {
+    cy.get('.products article').first().within(() => {
+      cy.get('a').click();
+    });
+    cy.url().should('include', '/products/');
   });
 })
